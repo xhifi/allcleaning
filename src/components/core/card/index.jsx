450975677cfe50b0c2b9910041eab24e8b03c5a1
xhifi <shifa.newversion@gmail.com>
2023-02-15 22:05:07 +0500

    Changed Card layout to receive prop

--- src/components/core/card/index.jsx
+++ src/components/core/card/index.jsx
@@ -1,39 +1,15 @@
 import CardLayout from "./CardLayout";
-import Card from "./Card";
+import CCard from "./Card";
 
-const dataMock = [
-  {
-    type: "with-image",
-    link: {
-      path: "/",
-      text: "Home",
-    },
-    img: {
-      url: "https://bityl.co/HBCz",
-      alt: "A Painting",
-    },
-    title: "My Custom Card",
-    description: `This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.`,
-  },
-  {
-    type: "with-icon",
-    link: {
-      path: "/about",
-      text: "About",
-    },
-    icon: {
-      name: "airplane",
-    },
-    title: "My Icon Card",
-    description: `Cool ass lookin icons`,
-  },
-];
+export default function Card({ cardData }) {
+  if (cardData.length <= 1) {
+    return <CCard data={cardData[0]} />;
+  }
 
-export default function Cards() {
   return (
     <CardLayout>
-      {dataMock.map((v, i) => {
-        return <Card key={i} data={v} />;
+      {cardData.map((v, i) => {
+        return <CCard key={i} data={v} />;
       })}
     </CardLayout>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,39 @@
-import Cards from "@/components/core/card";
+import Card from "@/components/core/card";
 
 import MainLayout from "@/components/layouts/Main";
+
+const cardsData = [
+  {
+    type: "with-image",
+    link: {
+      path: "/",
+      text: "Home",
+    },
+    img: {
+      url: "https://bityl.co/HBCz",
+      alt: "A Painting",
+    },
+    title: "My Custom Card",
+    description: `This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.`,
+  },
+  {
+    type: "with-icon",
+    link: {
+      path: "/about",
+      text: "About",
+    },
+    icon: {
+      name: "airplane",
+    },
+    title: "My Icon Card",
+    description: `Cool ass lookin icons`,
+  },
+];
+
 export default function Home() {
   return (
     <MainLayout>
-      <Cards />
+      <Card cardData={cardsData} />
     </MainLayout>
   );
 }
