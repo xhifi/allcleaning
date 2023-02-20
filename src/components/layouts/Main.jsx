5f7e07165b32bfced519f2c61483bfb2accdd5bc
xhifi <shifa.newversion@gmail.com>
2023-02-20 09:31:39 +0500

    a

--- src/components/layouts/Main.jsx
+++ src/components/layouts/Main.jsx
@@ -13,7 +13,11 @@ export default function MainLayout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin={true}
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Signika+Negative:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
--- src/pages/index.js
+++ src/pages/index.js
@@ -48,30 +48,95 @@ const singleObjectData = {
 export default function Home() {
   return (
     <MainLayout>
-      <Card cardData={cardsData} cols={[1, , 4, 6]} gutter="5" />
-      <Featured
-        img={{
-          src: "https://plus.unsplash.com/premium_photo-1661320897741-0924d7b88078?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
-          quality: "70",
-          fill: "cover",
-          className: "rounded-5 shadow-lg border-3 border border-white",
-        }}
-        btn={{
-          action: "/",
-          text: "Home",
-          icon: "airplane",
-          className: "btn btn-lg btn-light btn-shadow",
-        }}
-        title="My Featured Content"
-        className="bg-warning"
-      >
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad veniam
-        cupiditate, quibusdam id dicta excepturi saepe aut pariatur, laudantium,
-        iure unde adipisci suscipit harum consequuntur debitis recusandae
-        dolorem et incidunt nam aperiam sed omnis illo? Nam quisquam itaque
-        veritatis, nihil nobis doloribus reiciendis odit nulla debitis ipsum
-        eaque neque quod!
-      </Featured>
+      <Card cardData={cardsData} cols={[1, undefined, 4, 6]} gutter="5" />
+
+      {featureMock.map((item, i) => {
+        return (
+          <Featured
+            img={item.img}
+            btn={item.btn}
+            title={item.title}
+            className={item.className}
+            content={item.content}
+          >
+            {item.children}
+          </Featured>
+        );
+      })}
     </MainLayout>
   );
 }
+
+const featureMock = [
+  {
+    img: {
+      src: "https://plus.unsplash.com/premium_photo-1661320897741-0924d7b88078?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+      quality: "70",
+      fill: "cover",
+      className: "rounded-5 shadow-lg border-3 border border-white",
+    },
+    btn: {
+      action: "/",
+      text: "Home",
+      icon: "airplane",
+      className: "btn btn-lg btn-light btn-shadow",
+    },
+    title: "My Featured Content",
+    className: "bg-warning",
+  },
+  {
+    // img: {
+    //   src: "https://images.unsplash.com/photo-1676583821205-b9f79e3ca783?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+    //   quality: "70",
+    //   fill: "cover",
+    //   className: "rounded-5 shadow-lg border-3 border border-white",
+    //   position: "left",
+    // },
+    btn: {
+      action: "/",
+      text: "Home",
+      icon: "airplane",
+      className: "btn btn-lg btn-light btn-shadow",
+    },
+    title: "My Featured Content",
+    className: "bg-warning",
+    content: (
+      <div>
+        <p>This be the naughty naughty tickle ticklish</p>
+        <hr />
+      </div>
+    ),
+  },
+  {
+    img: {
+      src: "https://images.unsplash.com/photo-1612971446746-b79cc11824f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
+      quality: "70",
+      fill: "cover",
+      className: "rounded-5 shadow-lg border-3 border border-white",
+    },
+    btn: {
+      action: "/",
+      text: "Home",
+      icon: "airplane",
+      className: "btn btn-lg btn-light btn-shadow",
+    },
+    title: "My Featured Content",
+    className: "bg-warning",
+  },
+  {
+    img: {
+      src: "https://images.unsplash.com/photo-1612971446746-b79cc11824f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
+      quality: "70",
+      fill: "cover",
+      className: "rounded-5 shadow-lg border-3 border border-white",
+    },
+    btn: {
+      action: "/",
+      text: "Home",
+      icon: "airplane",
+      className: "btn btn-lg btn-light btn-shadow",
+    },
+    title: "My Featured Content",
+    className: "bg-warning",
+  },
+];
